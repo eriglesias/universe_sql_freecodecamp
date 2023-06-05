fd9402febeb278f2437fc64399db8509af8f3ba8
[Enrique Viveros] <encig@icloud.com>
2023-06-05 11:00:34 -0500

    updating file, adding extra files to the tables and fixes for the freecodecamp tests

--- universe.sql
+++ universe.sql
@@ -48,9 +48,10 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.asteroid (
-    name text,
-    diameter integer,
-    discovery_year integer
+    name character varying NOT NULL,
+    diameter integer NOT NULL,
+    discovery_year integer,
+    asteroid_id text NOT NULL
 );
 
 
@@ -63,7 +64,7 @@ ALTER TABLE public.asteroid OWNER TO freecodecamp;
 CREATE TABLE public.galaxy (
     galaxy_types character varying(60) NOT NULL,
     name_of_galaxy character varying(30),
-    galaxy_id integer,
+    galaxy_id integer NOT NULL,
     name character varying(60),
     is_in_universe boolean
 );
@@ -76,11 +77,11 @@ ALTER TABLE public.galaxy OWNER TO freecodecamp;
 --
 
 CREATE TABLE public.moon (
-    parent character varying(60) NOT NULL,
-    mean_radius numeric(5,3),
+    moon_id character varying(60) NOT NULL,
+    mean_radius numeric(5,3) NOT NULL,
     sidereal_period_d double precision,
     discovery_year integer,
-    name text,
+    name character varying,
     constellation character varying(60)
 );
 
@@ -92,10 +93,10 @@ ALTER TABLE public.moon OWNER TO freecodecamp;
 --
 
 CREATE TABLE public.planet (
-    satelites integer,
+    satelites integer NOT NULL,
     avg_orbital_speed_km_s double precision,
     surface_pressure_atm double precision,
-    type character varying(60) NOT NULL,
+    planet_id character varying(60) NOT NULL,
     name character varying(30),
     potential_life boolean,
     constellation character varying(60)
@@ -109,8 +110,8 @@ ALTER TABLE public.planet OWNER TO freecodecamp;
 --
 
 CREATE TABLE public.star (
-    type character varying(60) NOT NULL,
-    constellation character varying(60),
+    star_id character varying(60) NOT NULL,
+    constellation character varying(60) NOT NULL,
     rotation_days integer,
     name character varying(30),
     is_in_solar_system boolean,
@@ -124,11 +125,11 @@ ALTER TABLE public.star OWNER TO freecodecamp;
 -- Data for Name: asteroid; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.asteroid VALUES ('Eros', 34, 1898);
-INSERT INTO public.asteroid VALUES ('Vesta', 525, 1807);
-INSERT INTO public.asteroid VALUES ('Ceres', 939, 1801);
-INSERT INTO public.asteroid VALUES ('Pallas', 582, 1802);
-INSERT INTO public.asteroid VALUES ('Hygiea', 430, 1849);
+INSERT INTO public.asteroid VALUES ('Eros', 34, 1898, 'aa');
+INSERT INTO public.asteroid VALUES ('Vesta', 525, 1807, 'paa');
+INSERT INTO public.asteroid VALUES ('Ceres', 939, 1801, 'pbaa');
+INSERT INTO public.asteroid VALUES ('Pallas', 582, 1802, 'pbcaa');
+INSERT INTO public.asteroid VALUES ('Hygiea', 430, 1849, 'phhhha');
 
 
 --
@@ -152,6 +153,21 @@ INSERT INTO public.moon VALUES ('Jupiter', 12.000, 0.66, 16, 'Io', 'Cygnus');
 INSERT INTO public.moon VALUES ('Saturn', 76.000, 0.66, 16, 'Enceladus', 'Ursa Major');
 INSERT INTO public.moon VALUES ('Mars', 11.000, 0.3, 96, 'Phobos', 'Scorpius');
 INSERT INTO public.moon VALUES ('Neptune', 13.000, 5, 18, 'Triton', 'Aquarius');
+INSERT INTO public.moon VALUES ('Pandora', 1.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Gaia', 2.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Lyra', 3.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Celestia', 4.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Nova', 5.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Aether', 6.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Orion', 7.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Zenith', 8.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Seraph', 9.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Stella', 11.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Aurora', 12.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Luna', 13.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Phoenix', 14.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Vega', 15.000, NULL, NULL, NULL, NULL);
+INSERT INTO public.moon VALUES ('Solara', 72.000, NULL, NULL, NULL, NULL);
 
 
 --
@@ -163,6 +179,13 @@ INSERT INTO public.planet VALUES (79, 13.1, 1, 'Gas Giant', 'Jupiter', false, 'O
 INSERT INTO public.planet VALUES (1, 35.2, 0.1, 'Extra Terrestrial', 'Earth', true, 'Cygnus');
 INSERT INTO public.planet VALUES (14, 5.4, 1.5, 'Ice Giant', 'Neptune', false, 'Aquarius');
 INSERT INTO public.planet VALUES (19, 5.4, 1.5, 'Toxic', 'Venus', false, 'Ursa Major');
+INSERT INTO public.planet VALUES (2, 25, 9, 'Rocky', 'Astra', true, 'Pegasus');
+INSERT INTO public.planet VALUES (3, 12, 5, 'Oceanic', 'Aqua', false, 'Leo');
+INSERT INTO public.planet VALUES (1, 30, 8, 'Arid', 'Vulcan', false, 'Gemini');
+INSERT INTO public.planet VALUES (4, 18, 4, 'Desert', 'Sahara', false, 'Taurus');
+INSERT INTO public.planet VALUES (1, 16, 7, 'Volcanic', 'Inferno', true, 'Sagittarius');
+INSERT INTO public.planet VALUES (2, 22, 6, 'Jungle', 'Zephyr', false, 'Capricorn');
+INSERT INTO public.planet VALUES (5, 14, 3, 'Barren', 'Obsidian', false, 'Virgo');
 
 
 --
@@ -175,6 +198,13 @@ INSERT INTO public.star VALUES ('K3I', 'Ursa Major', 28, 'Alkaid', false, 'whirl
 INSERT INTO public.star VALUES ('O9V', 'Scorpius', 17, 'Antares', false, 'sombrero galaxy');
 INSERT INTO public.star VALUES ('MOV', 'Aquarius', 34, 'Aldebaran', false, 'centaurus A');
 INSERT INTO public.star VALUES ('OOO', 'Exclusive', 72, 'TUMAD', false, 'unknown');
+INSERT INTO public.star VALUES ('AAA', 'Pegasus', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('BBB', 'Leo', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('CCC', 'Gemini', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('DDD', 'Taurus', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('EEE', 'Sagittarius', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('FFF', 'Capricorn', NULL, NULL, NULL, NULL);
+INSERT INTO public.star VALUES ('GGG', 'Virgo', NULL, NULL, NULL, NULL);
 
 
 --
@@ -186,11 +216,27 @@ ALTER TABLE ONLY public.asteroid
 
 
 --
+-- Name: asteroid asteroid_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.asteroid
+    ADD CONSTRAINT asteroid_pkey PRIMARY KEY (asteroid_id);
+
+
+--
 -- Name: galaxy galaxy_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.galaxy
     ADD CONSTRAINT galaxy_name_key UNIQUE (name);
+
+
+--
+-- Name: galaxy galaxy_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
 
 
 --
@@ -222,7 +268,7 @@ ALTER TABLE ONLY public.star
 --
 
 ALTER TABLE ONLY public.moon
-    ADD CONSTRAINT panet_parent PRIMARY KEY (parent);
+    ADD CONSTRAINT panet_parent PRIMARY KEY (moon_id);
 
 
 --
@@ -230,7 +276,7 @@ ALTER TABLE ONLY public.moon
 --
 
 ALTER TABLE ONLY public.planet
-    ADD CONSTRAINT type_of_planet PRIMARY KEY (type);
+    ADD CONSTRAINT type_of_planet PRIMARY KEY (planet_id);
 
 
 --
@@ -238,15 +284,7 @@ ALTER TABLE ONLY public.planet
 --
 
 ALTER TABLE ONLY public.star
-    ADD CONSTRAINT type_of_start PRIMARY KEY (type);
-
-
---
--- Name: galaxy types_of_galaxy; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.galaxy
-    ADD CONSTRAINT types_of_galaxy PRIMARY KEY (galaxy_types);
+    ADD CONSTRAINT type_of_start PRIMARY KEY (star_id);
 
 
 --
